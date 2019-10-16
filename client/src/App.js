7f36afe3e7eb974b312a152baf46ec25ba71ed4f
AnastasiiaAleksandrova <47037922+AnastasiiaAleksandrova@users.noreply.github.com>
2019-10-16 22:39:58 +0300

    Multiple files uploading and improvements

--- client/src/App.js
+++ client/src/App.js
@@ -1,122 +1,227 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import ProgressBar from 'react-bootstrap/ProgressBar';
 import axios from 'axios';
 import './App.css';
+
+const uuidv4 = require('uuid/v4');
+
+const FAILED_TO_FETCH_FILE_LIST = 0;
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      uploaded: [],
-      progress: 0
+      uploading: new Map(),
+      files: [],
+      errors: new Map(),
+      
     };
-
-    this.uploadFile = this.uploadFile.bind(this);
     this.updateFilesList = this.updateFilesList.bind(this);
-    this.handleUpload = this.handleUpload.bind(this);
-    this.handleUpload = this.handleUpload.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
+    this.downloadFile = this.downloadFile.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
+    this.deleteFile = this.deleteFile.bind(this);
   }
-
-  updateFilesList() { 
-    try {
-      fetch('/upload')
-      .then(res => res.json())
-      .then(data => this.setState({uploaded: data}))
-    } catch(err) {
-      console.log(err);
-    }
-  };
 
   async uploadFile(file) {
     const data = new FormData();
     data.append('file', file);
+
+    const id = uuidv4();
+
     try {
-      let res = await axios.post('/upload', data, {
-        onUploadProgress: e => this.setProgress(e.loaded / e.total * 100)
-      })
+      this.createUploadProgress(id, file.name);
+
+      let res = await axios.post(`/file/${id}`, data, {
+        onUploadProgress: e => {
+          let progress = e.loaded / e.total * 100;
+          this.updateUploadProgress(id, progress);
+        }
+      });
 
       if (res.status === 200) {
-        this.setProgress(100);
-        console.log("getting files")
-        this.updateFilesList();
-        setTimeout(() => this.setProgress(0), 500);
+        this.updateUploadProgress(id, 100);
+        setTimeout(() => {
+          this.deleteUploadProgress(id);
+          this.updateFilesList();
+        }, 500);
       } else {
-        console.log("not ok")
+        this.showError(id, `Failed to upload file (${res.status})`);
       }
-
     } catch(err) {
-      console.log(err);
-    } 
+      this.showError(id, `Failed to upload file (${err})`);
+    }
   }
 
-  setProgress(progress) {
-    this.setState({progress: progress});
+  async getFileList() {
+    let res = await fetch('/files');
+    if (res.status == 200) {
+      return res.json();
+    }
+
+    throw Error(`Failed to fetch file list (${res.status})`);
   }
 
-  handleUpload(event) {
-   this.uploadFile(event.target.files[0]);
-  };
-
-  handleDownload(file) {
-    window.open(`/download/${file}`);
-  }
-
-  async handleDelete(file) {
+  async updateFilesList() {
     try {
-      let res = await axios.delete(`/upload/${file}`);
-      if (res.status === 204) {
+        let fileList = await this.getFileList();
+        fileList.sort((i0, i1) => i1.tstamp - i0.tstamp);
+        this.setState({files: fileList});
+    } catch (err) {
+      this.showFailedToFetchFileListError(err.toString());
+    }
+  }
+
+  createUploadProgress(id, name) {
+    let uploading = new Map(this.state.uploading);
+    uploading.set(id, {id: id, name: name, progress: 0});
+    this.setState({uploading: uploading});
+  }  
+
+  updateUploadProgress(id, progress) {
+    let item = this.state.uploading.get(id);
+    item.progress = progress;
+
+    let uploading = new Map(this.state.uploading);
+    uploading.set(id, item);
+    this.setState({uploading: uploading});
+  }
+
+  deleteUploadProgress(id) {
+    let uploading = new Map(this.state.uploading);
+    uploading.delete(id);
+    this.setState({uploading: uploading});
+  }
+
+  showError(id, err) {
+    let errors = new Map(this.state.errors);
+    errors.set(id, err);
+    this.setState({errors: errors});
+  }
+
+  clearError(id) {
+    let errors = new Map(this.state.errors);
+    errors.delete(id);
+    this.setState({errors: errors});
+  }
+
+  downloadFile(id) {
+    window.open(`/file/${id}`);
+  }
+
+  showFailedToFetchFileListError(error) {
+    this.showError(FAILED_TO_FETCH_FILE_LIST, error);
+  }
+
+  async deleteFile(id) {
+    try {
+      let res = await axios.delete(`/file/${id}`);
+      console.log(res.status)
+      if (res.status === 200) {
         this.updateFilesList();
       } else {
-        console.log('no')
+        this.showError(id, `Failed to delete: ${res.status}`);
       }
-    } catch(err) {
-      console.log(err);
-    } 
-  };
+    } catch (err) {
+      this.showError(id, `Failed to delete: ${err}`);
+    }
+  }
 
   componentDidMount() {
     this.updateFilesList();
-  };
+  }
 
   render() {
-    let progressView = (this.state.progress > 0) 
-      ? (<ProgressBar variant="success" now={this.state.progress} />)
-      : "";
     return (
       <div className='container'>
        
-        <input style={{ margin: '1em .3em'}} type="file" onChange={this.handleUpload}></input> 
-        <div className='progressbar'>
-          {progressView}
-        </div>
-        
-        {this.state.uploaded.map((item, index) =>
+        <input type='file' style={{margin: '1em 0'}}
+          onChange={e => {
+            this.uploadFile(e.target.files[0]);
+            e.target.value = null;
+          }}/> 
 
-          <Card key={index} style={{ margin: '.3em', padding: '.3em' }}>
+        {Array.from(this.state.uploading.values()).map(item => this.renderUploadItem(item))}
 
-            <Card.Text>{item}</Card.Text>
-
-            <div className='buttons mr-auto'>
-              <Button variant='light' className='mx-1' onClick={() => this.handleDownload(item)}>
-                <FontAwesomeIcon icon={faDownload}/>
-              </Button>
-              <Button variant='light' className='mx-1' onClick={() => this.handleDelete(item)}>
-                <FontAwesomeIcon icon={faTrashAlt}/>
-              </Button>
-            </div>
-
-          </Card>)}
-    
+        {this.renderFileList()}
       </div>
     );
-  }; 
-};
+  }
+
+  renderFileList() {
+    let error = this.state.errors.get(FAILED_TO_FETCH_FILE_LIST);
+    if (typeof error === 'undefined') {
+      return this.state.files.sort().map(item => this.renderFileItem(item));
+    } else {
+      return (
+        <Alert variant='danger'>
+          Failed to fetch uploaded files
+        </Alert>
+
+      )
+    }
+  }
+
+  renderUploadItem(item) {
+    let error = this.state.errors.get(item.id);
+    if (typeof error === 'undefined') {
+      return (
+        <Card key={item.id} style={{ margin: '.3em 0', padding: '.3em' }}>
+  
+          <Card.Text>{item.name}</Card.Text>
+  
+          <div>
+            <ProgressBar variant='success' now={item.progress} />
+          </div>
+      </Card>);
+    } else {
+      return this.renderErrorItem(item, 'Failed to upload', () => this.deleteUploadProgress(item.id));
+    }
+  }
+
+  renderErrorItem(item, error, handler) {
+    return (
+      <Card key={item.id} style={{ margin: '.3em 0', padding: '.3em' }}>
+
+        <Card.Text>{item.name}</Card.Text>
+
+        
+        <Alert variant='danger' onClose={() => handler(item.id)} dismissible>
+          {error}
+        </Alert>
+        
+
+
+    </Card>);
+  }
+
+  renderFileItem(item) {
+    let error = this.state.errors.get(item.id);
+    if (typeof error === 'undefined') {
+      return (
+        <Card key={item.id} style={{ margin: '.3em 0', padding: '.3em' }}>
+  
+          <Card.Text>{item.name}</Card.Text>
+  
+          <div className='buttons mr-auto'>
+            <Button variant='light' className='mx-1' onClick={() => this.downloadFile(item.id)}>
+              <FontAwesomeIcon icon={faDownload}/>
+            </Button>
+            <Button variant='light' className='mx-1' onClick={() => this.deleteFile(item.id)}>
+              <FontAwesomeIcon icon={faTrashAlt}/>
+            </Button>
+          </div>
+      </Card>);
+    } else {
+      return this.renderErrorItem(item, 'Failed to delete', () => this.clearError(item.id));
+    }
+  }
+}
 
 export default App;
--- index.js
+++ index.js
@@ -1,69 +1,96 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-const storage = multer.diskStorage({
-    destination: path.join(__dirname, 'uploads'),
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
-      }
-});
-
-const upload = multer({ storage: storage }).single('file');
-
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+});
 
-app.post('/upload', (req, res) => {
-    upload(req, res, function (err) {
-        if (err instanceof multer.MulterError) {
-            console.log(err)
-        } else if (err) {
-            console.log(err);
-        }
-        console.log("OK")
+const FILE_DIR = './uploads';
+
+const fileStorage = multer.diskStorage({
+    destination: path.join(__dirname, FILE_DIR),
+    filename: function (req, file, cb) {
+        let item = { id: req.params.id, name: file.originalname, tstamp: new Date().getTime()};
+        storage.set(item.id, item);
+        cb(null, `${buildFileName(item.tstamp, req.params.id, file.originalname)}`);
+      }
+});
+
+const fileUpload = multer({ storage: fileStorage }).single('file');
+
+let storage = new Map();
+{ 
+    fs.readdir(FILE_DIR, (err, files) => {
+        files.map(name => 
+            ({ id: name.substring(13, 49), tstamp: name.substring(0, 13), name: name.substring(49) }) 
+        ).forEach(item => {
+            filepool.set(item.id, item);
+        });
     });
-    res.sendStatus(200);
+}
+
+
+app.get('/files', (req, res) => {
+    res.send(Array.from(storage.values()));
+    //res.sendStatus(500);  // ERROR TESTING
 });
 
-app.get('/upload', (req, res) => {
-    fs.readdir('./uploads', (err, files) => {
-        res.send(files);
-     });
+app.post('/file/:id', (req, res) => {
+    let id = req.params.id;
+    if (!storage.has(id)) { 
+    //if (!filepool.has(id) && false) {  // ERROR TESTING
+        fileUpload(req, res, err => {
+            if (!err) {
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(500);
+            } 
+        })
+    } else { 
+        res.sendStatus(400); 
+    }
 });
 
-app.delete('/upload/:file', (req, res) => {
-    const file = path.join(__dirname, 'uploads', req.params.file);
-    fs.unlink(file, function() {
-        res.sendStatus(204);
-    });
-   
+app.delete('/file/:id', (req, res) => {
+    let item = storage.get(req.params.id);
+    if (typeof item !== 'undefined') {   
+    //if (typeof item !== 'undefined' && false) {  // ERROR TESTING
+        const file = path.join(__dirname, FILE_DIR, 
+            buildFileName(item.tstamp, item.id, item.name));
+        fs.unlinkSync(file);
+        
+        storage.delete(item.id);
+        
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
-app.get('/download/:file', (req, res) => {
-    const file = path.join(__dirname, 'uploads', req.params.file);
-    try {
-        console.log(file)
-        res.download(file, function(err) {
+app.get('/file/:id', (req, res) => {
+    let item = storage.get(req.params.id);
+    if (typeof item !== 'undefined') {
+        let fileName = buildFileName(item.tstamp, item.id, item.name)
+        console.log(item.name);
+        res.download(path.join(__dirname, FILE_DIR, fileName), item.name, err => { 
             if (err) {
                 console.log(err)
-            } else {
-                console.log('OK')
-            }
+            } 
         });
-        
-    } catch(err) {
-        console.log(err)
-    };
-    
+    } else {
+        res.sendStatus(404);
+    }
 });
+
+function buildFileName(tstamp, id, name) {
+    return `${tstamp}${id}${name}`;
+}
+
 
 app.listen(port, () => {console.log(`Server is listening on port ${port}`)});
